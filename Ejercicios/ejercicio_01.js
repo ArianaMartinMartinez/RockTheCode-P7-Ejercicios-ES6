7d731c1aa46731b36c8aa640ad13ff9f5f985b45
Ariana Martin <arianamartinmartinez@gmail.com>
2024-07-05 19:42:08 +0200

    Ejercicio 1 hecho

--- Ejercicios/ejercicio_01.js
+++ Ejercicios/ejercicio_01.js
@@ -2,6 +2,15 @@
 que por defecto el valor de a = 10 y de b = 5.
 Haz que la función muestre por consola la suma de los dos parametros.*/
 
+const sumNumbers = (a=10, b=5) => {
+    console.log(a+b);
+};
+
 //1.1 Ejecuta esta función sin pasar ningún parametro
+sumNumbers();
+
 //1.2 Ejecuta esta función pasando un solo parametro
+sumNumbers(8);
+
 //1.3 Ejecuta esta función pasando dos parametros
+sumNumbers(8, 25);
