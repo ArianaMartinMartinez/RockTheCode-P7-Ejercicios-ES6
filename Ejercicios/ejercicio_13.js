4e6d4389728c6f4141dbd85b5ff576207ba727c4
Ariana Martin <arianamartinmartinez@gmail.com>
2024-07-11 20:14:56 +0200

    Ejercicio 13 hecho

--- Ejercicios/ejercicio_13.js
+++ Ejercicios/ejercicio_13.js
@@ -9,3 +9,30 @@ Finalmente retorna el array.
 
 De nuevo haz varios ejemplos para practicar y comprueba que funcionan 
 correctamente.*/
+
+const mainCharacters = [
+    "Luke",
+    "Leia",
+    "Han Solo",
+    "Chewbacca",
+    "Rey",
+    "Anakin",
+    "Obi-Wan",
+];
+  
+function findArrayIndex(array, text) {
+    return array.indexOf(text);
+}
+
+function removeItem(array, text) {
+    const index = findArrayIndex(array, text);
+    if(index !== -1) {
+        array.splice(index, 1);
+    }
+
+    return array;
+}
+
+console.log(removeItem(mainCharacters, "Anakin"));
+console.log(removeItem(mainCharacters, "Luke"));
+console.log(removeItem(mainCharacters, "Darth Vader"));
